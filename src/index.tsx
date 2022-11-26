e21f7923c2aec0d8621b095a764c18074a5c363e
GetPsyched <priyanshutr@gmail.com>
2022-11-27 00:42:41 +0530

    Fix: Content-Type prone to causing errors

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 axios.defaults.baseURL = `https://db-viewer-server.up.railway.app`;
-axios.defaults.headers.post["Content-Type"] = "application/vnd.api+json";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
