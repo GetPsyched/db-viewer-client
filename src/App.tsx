b6d5e7d54673ac715806bca9595dabf776a445d9
GetPsyched <priyanshutr@gmail.com>
2022-11-27 02:00:47 +0530

    Add base form logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import logo from './assets/logo.svg';
 import './App.css';
 
 function App() {
+  const [dsn, setDsn] = React.useState(false)
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      dsn: { value: string };
+      query: { value: string };
+    };
+    const dsn = target.dsn.value;
+    const query = target.query.value;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        { dsn ? null : <img src={logo} className='App-logo' alt='logo' /> }
+        <form onSubmit={onSubmit}>
+          { dsn ? null : <input name='dsn' placeholder='DSN' type={'url'} required /> }
+          <input name='query' placeholder='SELECT col FROM table_name' type={'text'} required />
+          <button type='submit'>Fetch</button>
+        </form>
       </header>
     </div>
   );
