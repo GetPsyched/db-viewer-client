142e1fdf1bf32fa3cc6404e6d9b549e47f516f66
GetPsyched <priyanshutr@gmail.com>
2022-12-07 23:34:53 +0530

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,18 @@
-import axios from "axios";
-import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.scss";
-import Table from "./components/Table/Table";
-import DatabaseTreeView from "./components/DatabaseTreeView";
+import Home from "./components/Home";
+import VisualMagic from "./components/VisualMagic";
 
 function App() {
-  const [dsn, setDsn] = React.useState("");
-
-  const [dbTree, setDbTree] = React.useState(null);
-  const [data, setData] = React.useState(null);
-
-  const onSubmitDSN = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const target = event.target as typeof event.target & {
-      dsn: { value: string };
-    };
-    const dsn = target.dsn.value;
-    setDsn(dsn);
-
-    axios
-      .post("/schema", { dsn: dsn })
-      .then((res) => setDbTree(res.data))
-      .catch((err: Error) => {
-        console.error(err.message);
-      });
-  };
-
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const target = event.target as typeof event.target & {
-      query: { value: string };
-    };
-    const query = target.query.value;
-
-    axios
-      .post("/", { dsn: dsn, query: query })
-      .then((res) => setData(res.data))
-      .catch((err: Error) => {
-        console.error(err.message);
-      });
-  };
-
   return (
-    <div className="App">
-      {/* Display div for the INFORMATION_SCHEMA tree */}
-      <div className="App-tree">
-        <div className="App-dsnForm">
-          <form onSubmit={onSubmitDSN}>
-            <input
-              name="dsn"
-              id="dsnInput"
-              placeholder="postgres[ql]://[username[:password]@][host[:port],]/database[?parameter_list]"
-              type={"url"}
-              required
-            />
-          </form>
-        </div>
-        {dbTree !== null ? <DatabaseTreeView dbTree={dbTree} /> : null}
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-
-      {/* Input div for the SQL query */}
-      <div className="App-query">
-        <form onSubmit={onSubmit}>
-          <textarea
-            name="query"
-            id="queryInput"
-            placeholder="SELECT column_name FROM table_name"
-            required
-          />
-          <button type="submit">Fetch</button>
-        </form>
-      </div>
-
-      {/* Output div for the resultant table */}
-      <div className="App-data">
-        {data !== null ? (
-          <Table data={data} columns={Object.keys(data[0])} />
-        ) : null}
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import * as React from "react";
+
+import "../App.scss";
+import Table from "../components/Table/Table";
+import DatabaseTreeView from "../components/DatabaseTreeView";
+import { Link } from "react-router-dom";
+
+function Home() {
+  const [dsn, setDsn] = React.useState("");
+
+  const [dbTree, setDbTree] = React.useState(null);
+  const [data, setData] = React.useState(null);
+
+  const onSubmitDSN = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      dsn: { value: string };
+    };
+    const dsn = target.dsn.value;
+    setDsn(dsn);
+
+    axios
+      .post("/schema", { dsn: dsn })
+      .then((res) => setDbTree(res.data))
+      .catch((err: Error) => {
+        console.error(err.message);
+      });
+  };
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      query: { value: string };
+    };
+    const query = target.query.value;
+
+    axios
+      .post("/", { dsn: dsn, query: query })
+      .then((res) => setData(res.data))
+      .catch((err: Error) => {
+        console.error(err.message);
+      });
+  };
+
+  return (
+    <div>
+      {/* Display div for the INFORMATION_SCHEMA tree */}
+      <div className="App-tree">
+        <div className="App-dsnForm">
+          <form onSubmit={onSubmitDSN}>
+            <input
+              name="dsn"
+              id="dsnInput"
+              placeholder="postgres[ql]://[username[:password]@][host[:port],]/database[?parameter_list]"
+              type={"url"}
+              required
+            />
+          </form>
+        </div>
+        {dbTree !== null ? <DatabaseTreeView dbTree={dbTree} /> : null}
+      </div>
+
+      {/* Input div for the SQL query */}
+      <div className="App-query">
+        <form onSubmit={onSubmit}>
+          <textarea
+            name="query"
+            id="queryInput"
+            placeholder="SELECT column_name FROM table_name"
+            required
+          />
+          <button type="submit">Fetch</button>
+        </form>
+      </div>
+
+      {/* Output div for the resultant table */}
+      <div className="App-data">
+        {data !== null ? (
+          <Table data={data} columns={Object.keys(data[0])} />
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
